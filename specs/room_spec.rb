11f922d26e5890ac6066b55687d358258868d02d
Serenity911 <silvia.simonassi01@gmail.com>
2019-11-30 14:27:21 +0000

    MVP

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,15 +1,78 @@
 require("minitest/autorun")
 require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require("pry")
+# binding.pry
 
 require_relative("../room")
+require_relative("../customer")
 
 
 class RoomTest < MiniTest::Test
 
     def setup
-      @room1 = Room.new("The blue room")
+      @room1 = Room.new("The blue room", 10)
+      @customer1 = Customer.new("Bob")
 
     end
+
+    def test_customers_booked_counter_is_zero_at_start()
+      assert_equal(0, @room1.customers_booked_counter)
+    end
+
+    def test_add_customer_to_room()
+      @room1.add_customer_to_room(@customer1)
+      assert_equal(1, @room1.customers_booked_counter)
+    end
+
+    def test_do_not_re_add_customer_if_already_in_the_room()
+      @room1.add_customer_to_room(@customer1)
+      @room1.add_customer_to_room(@customer1)
+      assert_equal(1, @room1.customers_booked_counter)
+    end
+
+    def test_remove_customer_from_room()
+      @room1.add_customer_to_room(@customer1)
+
+      @room1.remove_customer_from_room(@customer1)
+      assert_equal(0, @room1.customers_booked_counter)
+    end
+
+    def test_do_not_remove_customer_from_room_if_not_booked()
+      @room1.add_customer_to_room(@customer1)
+      @room1.remove_customer_from_room(@customer2)
+      assert_equal(1, @room1.customers_booked_counter)
+    end
+
+    def test_playlist_starts_empty()
+      assert_equal(0, @room1.songs_counter)
+    end
+
+    def test_add_song()
+      @room1.add_song(@song1)
+      assert_equal(1, @room1.songs_counter)
+    end
+
+    def test_remove_song()
+      @room1.add_song(@song1)
+      @room1.remove_song(@song1)
+      assert_equal(0, @room1.songs_counter)
+    end
+
+    def test_room_max_capacity()
+      assert_equal(10, @room1.max_capacity)
+    end
+
+    # def test_can_()
+    #
+    # end
+    #
+    # def test_can_add_customer_to_room_if_not_empty()
+    #
+    # end
+    #
+    # def test_cannot_add_customer_to_full_room()
+    #
+    # end
 
 end
--- weekend_homework.md
+++ weekend_homework.md
@@ -6,9 +6,9 @@ You have been approached to build software for a Karaoke bar. Specifically, they
 
 Your program should be test driven and should be able to:
 
-- Create rooms, songs and guests
-- Check in guests to rooms/Check out guests from rooms
-- Add songs to rooms
+v - Create rooms, songs and guests
+v - Check in guests to rooms/Check out guests from rooms
+v - Add songs to rooms
 
 ### Extensions
 
